6a4c0d9efe5db7532949ffeabc7fe7a4d5df4632
Joel <joelgeorgekal@gmail.com>
2023-08-10 20:55:38 +0530

    Fixed point cloud generation

--- src/dynamic_obstacles/costmap_publisher.py
+++ src/dynamic_obstacles/costmap_publisher.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import rospy
-# import pyzed.sl as sl
+import pyzed.sl as sl
 import math
 import numpy as np
 import sys
@@ -17,68 +17,71 @@ zed=None
 init_params=None
 runtime_parameters=None
 
-# def init_zed():
-#     global zed,init_params,runtime_parameters
-#     # Create a Camera object
-#     zed = sl.Camera()
+def init_zed():
+    # TODO:
+    #   HOW TO USE ZED SDK ALONG WITH ZED ROS WRAPPER
+    
+    global zed,init_params,runtime_parameters
+    # Create a Camera object
+    zed = sl.Camera()
 
-#     # Create a InitParameters object and set configuration parameters
-#     init_params = sl.InitParameters()
-#     init_params.depth_mode = sl.DEPTH_MODE.PERFORMANCE  # Use PERFORMANCE depth mode
-#     init_params.coordinate_units = sl.UNIT.METER  # Use meter units (for depth measurements)
-#     init_params.camera_resolution = sl.RESOLUTION.HD720
+    # Create a InitParameters object and set configuration parameters
+    init_params = sl.InitParameters()
+    init_params.depth_mode = sl.DEPTH_MODE.PERFORMANCE  # Use PERFORMANCE depth mode
+    init_params.coordinate_units = sl.UNIT.METER  # Use meter units (for depth measurements)
+    init_params.camera_resolution = sl.RESOLUTION.HD720
 
-#     # Open the camera
-#     err = zed.open(init_params)
-#     if err != sl.ERROR_CODE.SUCCESS:
-#         exit(1)
+    # Open the camera
+    err = zed.open(init_params)
+    if err != sl.ERROR_CODE.SUCCESS:
+        print("HAHA")
+        exit(1)
 
-#     # Create and set RuntimeParameters after opening the camera
-#     runtime_parameters = sl.RuntimeParameters()
-#     runtime_parameters.confidence_threshold = 100
-#     runtime_parameters.texture_confidence_threshold = 100
+    # Create and set RuntimeParameters after opening the camera
+    runtime_parameters = sl.RuntimeParameters()
+    runtime_parameters.confidence_threshold = 100
+    runtime_parameters.texture_confidence_threshold = 100
     
 
-# def get_depth(x,y):
-#     global zed,init_params,runtime_parameters
+def get_depth(x,y):
+    global zed,init_params,runtime_parameters
     
-#     depth = sl.Mat()
-#     point_cloud = sl.Mat()
+    depth = sl.Mat()
+    point_cloud = sl.Mat()
 
-#     mirror_ref = sl.Transform()
-#     mirror_ref.set_translation(sl.Translation(2.75,4.0,0))
-#     tr_np = mirror_ref.m
+    mirror_ref = sl.Transform()
+    mirror_ref.set_translation(sl.Translation(2.75,4.0,0))
+    tr_np = mirror_ref.m
     
-#     if zed.grab(runtime_parameters) == sl.ERROR_CODE.SUCCESS:
-#         # Retrieve depth map. Depth is aligned on the left image
-#         zed.retrieve_measure(depth, sl.MEASURE.DEPTH)
-            
-#         # Retrieve colored point cloud. Point cloud is aligned on the left image.
-#         # zed.retrieve_measure(point_cloud, sl.MEASURE.XYZRGBA)
+    if zed.grab(runtime_parameters) == sl.ERROR_CODE.SUCCESS:
+        # Retrieve depth map. Depth is aligned on the left image
+        zed.retrieve_measure(depth, sl.MEASURE.DEPTH)
+        zed.retrieve_measure(point_cloud, sl.MEASURE.XYZRGBA)
+        # Retrieve colored point cloud. Point cloud is aligned on the left image.
+        # zed.retrieve_measure(point_cloud, sl.MEASURE.XYZRGBA)
 
-#         # Get and print distance value in mm at the center of the image
-#         # We measure the distance camera - object using Euclidean distance
-#         err, point_cloud_value = point_cloud.get_value(x, y)
+        # Get and print distance value in mm at the center of the image
+        # We measure the distance camera - object using Euclidean distance
+        err, point_cloud_value = point_cloud.get_value(x, y)
 
-#         distance = math.sqrt(point_cloud_value[0] * point_cloud_value[0] +
-#                              point_cloud_value[1] * point_cloud_value[1] +
-#                              point_cloud_value[2] * point_cloud_value[2])
 
-#         point_cloud_np = point_cloud.get_data()
-#         point_cloud_np.dot(tr_np)
 
-#         if not np.isnan(distance) and not np.isinf(distance):
-#             return distance
-#         else:
-#             # If the point is too close to the camera
-#             return 0
-#         sys.stdout.flush()
+        distance = math.sqrt(point_cloud_value[0] * point_cloud_value[0] +
+                             point_cloud_value[1] * point_cloud_value[1] +
+                             point_cloud_value[2] * point_cloud_value[2])
 
-def publish_point_cloud(x,y,d):
+        point_cloud_np = point_cloud.get_data()
+        point_cloud_np.dot(tr_np)
+
+        if not np.isnan(distance) and not np.isinf(distance):
+            return distance
+        else:
+            # If the point is too close to the camera
+            return 0
+        sys.stdout.flush()
+def publish_point_cloud(points):
     point_cloud_pub = rospy.Publisher("segmentation/pointcloud", PointCloud2, queue_size=10)
     
-    rgba  = struct.unpack('I', struct.pack('BBBB', 255, 255, 255, 255))[0] 
-    points = [[x,i,d,rgba] for i in range(20)]
     header = Header()
     header.frame_id = "base_link"
     pc2 = point_cloud2.create_cloud(header, [PointField('x', 0, PointField.FLOAT32, 1),
@@ -95,20 +98,24 @@ def callback(data):
 
     sobelxy = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5) # Combined X and Y Sobel Edge Detection
 
-    
-
+    rgba  = struct.unpack('I', struct.pack('BBBB', 255, 255, 255, 255))[0]
+    points=[]
     for row in range(sobelxy.shape[0]):
         for column in range(sobelxy.shape[1]):
             if sobelxy[row][column] == 255:
-                #publish_point_cloud(column,row,get_depth(column,row))
-                publish_point_cloud(column,row,0)
+                depth=get_depth(column,row)
+                [points.append([column,depth,i,rgba]) for i in range(50)]
+
+    publish_point_cloud(points)
+    
+
     
 def main():
+    
     rospy.init_node('costmap_publisher', anonymous=True)
-    #init_zed()
+    init_zed()
     rospy.Subscriber = rospy.Subscriber("/zed2i/drivable_region",Image,callback)
     rospy.spin()
-    
         
 if __name__ == "__main__":
     main()
